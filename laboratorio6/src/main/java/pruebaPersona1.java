06f6140a25412c1718f7f86911d4a35552d57381
jham carlos temoche iman <temocheimanjhancarlos18@gmail.com>
2025-02-17 09:22:28 -0500

    creacion del la clase person

---
+++ laboratorio6/src/main/java/pruebaPersona1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author IDAT
+ */
+public class PruebaPersona1 {
+    public static void main(String[] args){
+        Persona persona = new Persona("juan carlos");
+        System.out.println();   
+        persona.name = "Nombre Modificado";
+        persona.setName("Nombre Modificado");
+    }
+}
